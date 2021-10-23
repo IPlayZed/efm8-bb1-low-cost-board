908d8b7618fc499c3485b38a32523a7e8b3b4cc0
KovacsKaliRobert <kovacskalirobert@gmail.com>
2021-10-23 17:51:53 +0200

    prell off

---
+++ led-button/ledButton1/Keil 8051 v9.60.0 - Debug/src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+A51_UPPER_SRCS += \
+C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/SILABS_STARTUP.A51 
+
+C_SRCS += \
+../src/InitDevice.c \
+../src/Interrupts.c \
+../src/ledButton1_main.c 
+
+OBJS += \
+./src/InitDevice.OBJ \
+./src/Interrupts.OBJ \
+./src/SILABS_STARTUP.OBJ \
+./src/ledButton1_main.OBJ 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.OBJ: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Compiler'
+	C51 "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/InitDevice.OBJ: C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/Users/kovac/Desktop/miclab-zh-1/efm8-bb1-low-cost-board/led-button/ledButton1/inc/InitDevice.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdbool.h
+
+src/Interrupts.OBJ: C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdbool.h
+
+src/SILABS_STARTUP.OBJ: C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/SILABS_STARTUP.A51
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Assembler'
+	AX51 "@$(patsubst %.OBJ,%.__ia,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/ledButton1_main.OBJ: C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/Users/kovac/Desktop/miclab-zh-1/efm8-bb1-low-cost-board/led-button/ledButton1/inc/InitDevice.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdbool.h
+
+
